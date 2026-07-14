4bdf41d696b4fe4dcc21d1bcc5200bcb5a24418b
descartavel-png <descartavel541@gmail.com>
2026-07-14 08:03:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post("/v1/chat/completions", async (req, res) => {
     const charPersonality = messages.find(m => m.role === 'system')?.content || "";
     
     const payload = {
-      model:"nvidia/nemotron-3-super-120b-a12b",
+      model:"nvidia/nemotron-3-ultra-550b-a55b",
       messages: [
         { role: "system", content: charPersonality }, // A personalidade SEMPRE inteira aqui
         { role: "system", content: `Resumo do histórico: ${summary}` },
