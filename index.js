49452e24b3f71cad42404327188c7a5ecf775f0d
descartavel-png <descartavel541@gmail.com>
2026-09-13 11:06:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post("/v1/chat/completions", async (req, res) => {
     const charPersonality = messages.find(m => m.role === 'system')?.content || "";
     
     const payload = {
-      model:"moonshotai/kimi-k3",
+      model:"deepseekai/deepseek-v4-pro-0813",
       messages: [
         { role: "system", content: charPersonality }, // A personalidade SEMPRE inteira aqui
         { role: "system", content: `Resumo do histórico: ${summary}` },
